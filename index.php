1b161a9e929dcc9650d88e2575887bd59a534701
Lotti <lotti86@gmail.com>
2015-04-26 23:42:49 +0200

    plugin done. documentation is still missing

--- index.php
+++ index.php
@@ -2,20 +2,25 @@
 require_once(__DIR__.'/libraries/vendor/phpFlickr/phpFlickr.php');
 
 define('__FLICKR_API_KEY__', 'c4ab144d6da31a15125de4a43f7585c2');
+define('__IMAGE_PER_PAGE__', 100);
 
 $flickr = new phpFlickr(__FLICKR_API_KEY__);
-$result = $flickr->photos_search(['text'=>'raccoon','tags'=>'raccoon','per_page'=>30]);
+$result = $flickr->photos_search(['text'=>'raccoon','tags'=>'raccoon','per_page'=>__IMAGE_PER_PAGE__]);
 
 ?>
 <html>
 <head>
+    <meta charset="utf-8">
     <title>Raccoon for everyone!</title>
     <link rel="stylesheet" href="/css/style.css" />
     <script type="text/javascript" language="JavaScript" src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
     <script type="text/javascript" language="JavaScript" src="/js/vendor/jquery/plugins/jquery.lazyload.js"></script>
     <script type="text/javascript" language="JavaScript">
         $(function () {
-           //plugin activation on document ready
+           $('.polaroids').lazyLoad({
+               selector: '.lazyload',
+               placeholder: '/img/placeholder.gif'
+           });
         });
     </script>
 </head>
@@ -27,7 +32,7 @@ foreach($result['photo'] as $p) {
         $p['title'] = substr($p['title'], 0, 17) . '...';
     }
 
-    echo '<li><a title="'.$p['title'].'" href="javascript:void(0);"><img class="lazy" src="http://farm'.$p['farm'].'.static.flickr.com/'.$p['server'].'/'.$p['id'].'_'.$p['secret'].'.jpg" alt="'.$p['title'].'"></a></li>'."\n";
+    echo '<li><a title="'.$p['title'].'" href="javascript:void(0);"><img class="lazyload" data-src="http://farm'.$p['farm'].'.static.flickr.com/'.$p['server'].'/'.$p['id'].'_'.$p['secret'].'.jpg" alt="'.$p['title'].'"></a></li>'."\n";
 }
 ?>
 </ul>
